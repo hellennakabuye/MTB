3bd2d8d73dde9f6a1aec5810aaaa11c5f08c1336
hellennakabuye <55150746+hellennakabuye@users.noreply.github.com>
2023-09-21 14:37:31 +0300

    Add files via upload

---
+++ file.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+mykrobe predict --sample my_sample_name   --species tb   --output /home/nakabuye/Desktop/mytb/tbone/media/result.csv   --format csv   --seq /home/nakabuye/Desktop/mytb/tbone/media/mtb.fastq
+
